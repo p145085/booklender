bd1e3b0c8430ae4570c1c3758f1afedf32c48c00
emila <emil.a.johansson@gmail.com>
2022-03-28 09:26:25 +0200

    Initial commit

---
+++ booklender/src/main/java/se/lexicon/emiljohansson/booklender/BooklenderApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.emiljohansson.booklender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklenderApplication.class, args);
+	}
+
+}
---
+++ booklender/src/test/java/se/lexicon/emiljohansson/booklender/BooklenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.emiljohansson.booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
